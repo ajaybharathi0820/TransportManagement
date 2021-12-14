82a0e011c840f6fab4c3abcbc4b6f1d16baecfcd
ajaybharathi0820 <92905774+ajaybharathi0820@users.noreply.github.com>
2021-12-14 20:42:37 +0530

    testing successful

--- Controllers/employeesController.cs
+++ Controllers/employeesController.cs
@@ -32,8 +32,8 @@ namespace SampleMvc.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "EmployeeId,EmployeeName,Password,Gender,DateOfBirth,DateOfJoining,PhoneNo")] employee employee)
         {
             if (ModelState.IsValid)
